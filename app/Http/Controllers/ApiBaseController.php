edcc9c050fbabfa06ecebb1c70104496438fc478
Carlos Ávila Aguilar <cavila@fiscaliaveracruz.gob.mx>
2018-03-08 21:20:20 -0600

    Se crearón dos metodos uno para generar un codigo alfanumerico de 6 caracteres y otro para generar el NUC

--- app/Http/Controllers/ApiBaseController.php
+++ app/Http/Controllers/ApiBaseController.php
@@ -4,12 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Nuc;
 
 class ApiBaseController extends Controller
 {
     public function validateTable($table)
     {
     	return Schema::hasTable('ctr_cipp');
+    }
+
+    public function getRandomCode(){
+        $an = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $su = strlen($an) - 1;
+        return substr($an, rand(0, $su), 1) .
+                substr($an, rand(0, $su), 1) .
+                substr($an, rand(0, $su), 1) .
+                substr($an, rand(0, $su), 1) .
+                substr($an, rand(0, $su), 1) .
+                substr($an, rand(0, $su), 1);
+    }    
+
+    public function calculateNuc()
+    {
+        $nuc = Nuc::orderby('nuc','DESC')->take(1)->pluck('nuc');
+        $numero = (int)substr($nuc,4,6);
+        $numero = $numero+1;
+        $numero = str_pad($numero, 6, "0", STR_PAD_LEFT);
+        return Carbon::now()->formatLocalized('%y').$numero.$this->getRandomCode();        
     }
 
     public function sendResponse($result, $message)
